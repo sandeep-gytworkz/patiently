12a8a22d9c9281e69ada74778baf832047699e78
riyazkhan2507 <riyaz2507@gmail.com>
2022-11-29 11:01:00 +0530

    terms modal change

---
+++ src/UI/Modal/TermsModal.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const TermsModal = () => {
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Terms & Conditions
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <p>Please Agree the terms & conditions to proceed further</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TermsModal;
