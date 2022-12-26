a707c2680d2eddadec61df4f71fa24925c99c300
Shivansh Pratap <shivanshpratap@gytworkz.com>
2022-12-26 20:15:53 +0530

    Changed Imports

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React, { createContext, useEffect, useReducer } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import "animate.css";
-import AboutUs from "./pages/About/About";
+
+import About from "./pages/About/About";
 import DataPrivacy from "./pages/DataPrivacy/DataPrivacy";
-import Homepage from "./pages/Home/Home";
-import LoginPage from "./pages/Login/Login";
-
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 import Participate from "./pages/Participate/Participate";
-import Signup from "./pages/SignUp/Signup";
-import Slide9 from "./pages/Profile/UserProfile";
-
+import UserProfile from "./pages/Profile/UserProfile";
 import VerifyOtp from "./pages/Login/VerifyOtp/VerifyOtp";
 import ResetPassword from "./pages/Login/ResetPassword/ResetPassword";
 import ForgotPasswordOtp from "./pages/Login/ForgotPassword/ForgotPasswordOtp";
 import ContactUs from "./pages/ContactUs/ContactUs";
 import HowItWorks from "./pages/HowItWorks/HowItWorks";
-import { reducer } from "./Redux/Reducers";
-import { initialState } from "./Redux/States";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Dashboard from "./pages/Admin/AdminDashboard/Dashboard";
 import Records from "./pages/Admin/Records/Records";
 import ParticipantProfile from "./pages/Admin/Records/ParticipantProfile/ParticipantProfile";
 import Participants from "./pages/Admin/Participants/Participants";
 import AccountManagement from "./pages/Admin/AccountManagement/AccountManagement";
 import AdminLogin from "./pages/Admin/AdminLogin/AdminLogin";
+
+import { reducer } from "./Redux/Reducers";
+import { initialState } from "./Redux/States";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export const AppContext = createContext();
 // export const DashboardContext = createContext();
@@ -73,55 +72,23 @@ function App() {
   return (
     <AppContext.Provider value={{ appState: state, appDispatch: dispatch }}>
       <Routes>
-        <Route exact path="/" element={<Homepage />} />
-        <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/about" element={<AboutUs />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
         <Route exact path="/how-it-works" element={<HowItWorks />} />
         <Route exact path="/contact-us" element={<ContactUs />} />
         <Route exact path="/participate-now" element={<Participate />} />
         <Route exact path="/data-privacy" element={<DataPrivacy />} />
-        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/login" element={<Login />} />
         <Route exact path="/admin-login" element={<AdminLogin />} />
         <Route exact path="/dashboard" element={<Dashboard />} />
         <Route exact path="/records" element={<Records />} />
-        <Route
-          exact
-          path="/participant-profile"
-          element={<ParticipantProfile />}
-        />
+        <Route exact path="/participant-profile" element={<ParticipantProfile />} />
         <Route exact path="/participants" element={<Participants />} />
-        <Route
-          exact
-          path="/account-management"
-          element={<AccountManagement />}
-        />
-        <Route
-          exact
-          path="/view-profile"
-          element={
-            <ProtectedRoute>
-              <Slide9 />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          exact
-          path="/verify-otp"
-          element={
-            <VerifyOtp
-              isForgotPassword={true}
-              changeSlide={() => {
-                navigate("/reset-password");
-              }}
-            />
-          }
-        />
+        <Route exact path="/account-management" element={<AccountManagement />} />
+        <Route exact path="/view-profile" element={<UserProfile />} />
+        <Route exact path="/verify-otp" element={ <VerifyOtp isForgotPassword={true} changeSlide={() => {navigate("/reset-password")}} />} />
         <Route exact path="/reset-password" element={<ResetPassword />} />
-        <Route
-          exact
-          path="/forgot-password-otp"
-          element={<ForgotPasswordOtp />}
-        />
+        <Route exact path="/forgot-password-otp" element={<ForgotPasswordOtp />} />
       </Routes>
     </AppContext.Provider>
   );
